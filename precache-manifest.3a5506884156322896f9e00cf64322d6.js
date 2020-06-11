80f26af16d23b5b1744df6053040633286fa93f9
Jeff Simmons <cyberkeyboarder@outlook.com>
2020-06-10 19:38:04 -0700

    Updates

---
+++ precache-manifest.3a5506884156322896f9e00cf64322d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac814330312a5a2eeee129ea27d84e3",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "8270e2db5517ae901096",
+    "url": "/monsters-react/static/css/main.49abd4d9.chunk.css"
+  },
+  {
+    "revision": "f0d0a20aebd87a848a56",
+    "url": "/monsters-react/static/js/2.6fcc70f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.6fcc70f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8270e2db5517ae901096",
+    "url": "/monsters-react/static/js/main.f1b93a69.chunk.js"
+  },
+  {
+    "revision": "51d5dc2958d61a59976f",
+    "url": "/monsters-react/static/js/runtime-main.fe6e3d3d.js"
+  }
+]);
